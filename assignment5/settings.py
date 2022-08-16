f9e8a8351202f14471b6a1c830fa97d9aca40aeb
StankovskiA <StankovskiA@gmail.com>
2022-08-17 00:13:05 +0200

    Fix settings.py

--- assignment5/settings.py
+++ assignment5/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
-
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +130,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_heroku
+
+django_heroku.settings(locals())
