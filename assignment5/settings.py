318280ad5fb920e7a616a2b2418d6e125eb1871a
StankovskiA <StankovskiA@gmail.com>
2022-08-17 00:22:15 +0200

    CSRF Trusted Origins

--- assignment5/settings.py
+++ assignment5/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+CSRF_TRUSTED_ORIGINS = ['https://guarded-castle-64489.herokuapp.com']
+
 STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 
